25aac03c34f13307d814c6d91d49d8cf8285a492
Felipe-Baamonde <felipe.baamonde@sptech.school>
2024-11-08 13:18:29 -0300

    Seller entity

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+        Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(),3070.90,department);
+        System.out.println(seller);
     }
 }
